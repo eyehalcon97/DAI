e81c7b954ac76d955d9f29a3dc6bdcad8dd8a424
Manuel Ortiz <gaditanodel97@gmail.com>
2021-11-07 00:37:24 +0100

    finalizando css

--- practica2/app/app.py
+++ practica2/app/app.py
@@ -15,7 +15,7 @@ app.secret_key = "vjepmewawfew"
 def index():
     nombre = request.cookies.get('nombre')
     pagina = 'Home'
-    link = '/'
+    link = 'index'
     visitado = leerhistorial()
     
     return enviarcookie(render_template('index.html',nombre=nombre,visitado=visitado),None,pagina,link)
@@ -24,22 +24,24 @@ def index():
 @app.errorhandler(404)
 def page_not_found(error):
     nombre = request.cookies.get('nombre')
+    visitado = leerhistorial()
     solucion = 'Pagina no encontrada, por favor, verifique el enlace'
-    #visitado = ultimavisita('error','','','')
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado)
 
 # Pagina estatica con imagen
 @app.route('/estatica')
 def gato():
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('estatica','gato','')
-    foto = '/static/images/gato.jpg alt="Gato"'
-    return render_template('gato.html',foto=foto,nombre=nombre)
+    pagina = 'Imagen Estatica'
+    link = 'gato'
+    visitado = leerhistorial()
+    foto = '/static/images/gato.jpg alt="Gato" '
+    return enviarcookie(render_template('gato.html',foto=foto,nombre=nombre,visitado=visitado),None,pagina,link)
 
 @app.route('/entrar' , methods=['GET', 'POST'] )
 def entrar():
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('entrar','entrar','')
+    visitado = leerhistorial()
     if request.method == 'POST':
         nombre = request.form['nombre']
         pws = request.form['pws']
@@ -47,51 +49,49 @@ def entrar():
         if pws == model.leerbd(nombre):
             solucion = 'Bienvenido : ' + str(nombre)
             
-            
-            return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre),nombre)
+            return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),nombre)
         else: 
             if model.exiteuser(nombre):
                 solucion = 'La contraseña es incorrecta'
-                return enviarcookie( render_template('entrar.html',solucion=solucion))
+                return render_template('entrar.html',solucion=solucion,visitado=visitado)
             else:
                 solucion = 'El usuario no existe'
-                return enviarcookie(render_template('entrar.html',solucion=solucion))
+                return render_template('entrar.html',solucion=solucion,visitado=visitado)
             
     else:
         if nombre == None:
-            return enviarcookie(render_template('entrar.html'),'')
+            return render_template('entrar.html',visitado=visitado)
         else:
             solucion = 'El usuario ya esta logueado '
-            return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre))
+            return render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado)
 
 @app.route('/registrar' , methods=['GET', 'POST'] )
 def registrar():
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('registrar','registrar','')
+    visitado = leerhistorial()
     if request.method == 'POST':
         nombre = request.form['nombre']
         psw = request.form['pws']
         
         if model.exiteuser(nombre):
             solucion = 'El usuario ya existe, Introduce un nuevo nombre de Usuario :'
-            return render_template('registrar.html',solucion=solucion)
+            return render_template('registrar.html',solucion=solucion,visitado=visitado)
         else:
             if model.nuevousuario(nombre,psw):
                 solucion = ' Se ha creado el usuario : ' + str(nombre)
-                resp = make_response(render_template('index.html',solucion=solucion,nombre=nombre))
-                resp.set_cookie('nombre',nombre)
-                return resp
+                return enviarcookie(make_response(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),nombre))
     else:
         if nombre == None:
             solucion = 'Registro de nuevo usuario : '
-            return render_template('registrar.html',solucion=solucion)
+            return render_template('registrar.html',solucion=solucion,visitado=visitado)
         else :
             solucion = 'El usuario ya esta logueado '
-            return render_template('index.html',solucion=solucion,nombre=nombre)
+            return render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado)
 
 @app.route('/modificar' , methods=['GET', 'POST'] )
 def modificar():
     nombre = request.cookies.get('nombre')
+    visitado = leerhistorial()
     password = model.leerbd(nombre)
     if request.method == 'POST':
         nombrenuevo = request.form['nombre']
@@ -99,26 +99,26 @@ def modificar():
         
         if nombre != nombrenuevo and model.exiteuser(nombrenuevo):
             solucion = 'El usuario ya existe, Introduce otro nombre de Usuario : '
-            return enviarcookie(render_template('modificar.html',solucion=solucion))
+            return render_template('modificar.html',solucion=solucion,visitado=visitado)
         else:
             if model.modificar(nombre,nombrenuevo,psw):
                 nombre = nombrenuevo
                 solucion = ' Se han modificado los datos del usuario : ' + str(nombre)
-                #resp = make_response(render_template('index.html',solucion=solucion,nombre=nombre))
-                #resp.set_cookie('nombre',nombre)
-                return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre),nombre)
+                return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),nombre)
     else:
         if nombre == None:
             solucion = 'No ha iniciado Sesion'
-            return enviarcookie(render_template('modificar.html',solucion=solucion))
+            return render_template('modificar.html',solucion=solucion,visitado=visitado)
         else :
             solucion = 'Modifique el Usuario '
-            return enviarcookie(render_template('modificar.html',solucion=solucion,nombre=nombre,psw=password),nombre)
+            return render_template('modificar.html',solucion=solucion,nombre=nombre,psw=password,visitado=visitado)
 
 @app.route('/pedirdatos/<cadena>' , methods=['GET', 'POST'] )
 def pedirdatos(cadena):
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('pedirdatos','pedirdatos',cadena)
+    pagina = 'Pedir datos de : ' + str(cadena)
+    link = 'pedirdatos'
+    visitado = leerhistorial()
     if request.method == 'POST':
         datos = request.form['datos']
         if cadena == 'ordenacion':
@@ -131,14 +131,16 @@ def pedirdatos(cadena):
             return valido(datos)
 
     else:    
-        return render_template('pedirdatos.html',cadena=cadena,nombre=nombre)
+        return enviarcookie(render_template('pedirdatos.html',cadena=cadena,nombre=nombre,visitado=visitado),None,pagina,link,cadena)
 
 
 # Pagina con dibujos aleatorios
 @app.route('/sgv')
 def sgv():
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('sgv','sgv','')
+    pagina = 'Pagina con SGV'
+    link = 'sgv'
+    visitado = leerhistorial()
     colores= ['red','green','blue', 'yellow','black']
     rectangulox = random.randint(0,500)
     rectanguloy = random.randint(0,500)
@@ -148,13 +150,15 @@ def sgv():
     elipsex = random.randint(0,500)
     elipsey = random.randint(0,500)
     elipsecol = random.choice(colores)
-    return render_template('sgv.html',recposx=recposx, recposy=recposy,rectangulox=rectangulox,rectanguloy=rectanguloy,rectangulocol=rectangulocol,elipsex=elipsex,elipsey=elipsey,elipsecol=elipsecol,nombre=nombre)
+    return enviarcookie(render_template('sgv.html',recposx=recposx, recposy=recposy,rectangulox=rectangulox,rectanguloy=rectanguloy,rectangulocol=rectangulocol,elipsex=elipsex,elipsey=elipsey,elipsecol=elipsecol,nombre=nombre,visitado=visitado),None,pagina,link)
 
 # Pagina que ordena los numeros
 @app.route('/ordena/<cadena>')
 def burbuja(cadena):
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('ordena','burbuja',cadena)
+    pagina = 'Ordena : ' + str(cadena)
+    link = 'burbuja'
+    visitado = leerhistorial()
     numeros= cadena.split(',')
     for numPasada in range(len(numeros)-1,0,-1):
         for i in range(numPasada):
@@ -165,13 +169,15 @@ def burbuja(cadena):
 
 
     solucion = 'La lista ordenada es : ' + str(numeros)
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),None,pagina,link,cadena)
 
 # Pagina de criba de Eratostenes
 @app.route('/criba/<cadena>')
 def criba(cadena):
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('criba','criba',cadena)
+    pagina = 'Criba : ' + str(cadena)
+    link = 'criba'
+    visitado = leerhistorial()
     lista = list(range(2,int(cadena)))
     for x in lista:
         for y in lista:
@@ -179,23 +185,28 @@ def criba(cadena):
                 lista.remove(y)
     lista.insert(0,2)
     solucion = 'Los numeros primos menores de : ' + cadena + ' son ' + str(lista) 
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),None,pagina,link,cadena)
 
 # Pagina de Fibonacci
 @app.route('/fibonacci/<cadena>')
 def fibonacci(cadena):
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('fibonacci','fibonacci',cadena)
+    pagina = 'fibbonacci : ' + str(cadena)
+    link = 'fibonacci'
+    visitado = leerhistorial()
     resultado = [ 0 , 1]
     for i in range(1,int(cadena)):
         resultado.append(resultado[i] + resultado[i-1])
     solucion = 'El numero ' + cadena + ' en la sucesion fibonacci es :' + str(resultado[int(cadena)])
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),None,pagina,link,cadena)
 
 # Pagina que indica si la cadena generada es valida
 @app.route('/cadena')
 def cadena():
     nombre = request.cookies.get('nombre')
+    pagina = 'Cadena'
+    link = 'cadena'
+    visitado = leerhistorial()
     #visitado = ultimavisita('cadena','cadena','')
     tamano = random.randint(2,10)
     resultado = []
@@ -221,36 +232,45 @@ def cadena():
     else:
         solucion ='La cadena generada es ' + str(resultado) + ' y es invalida'
 
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),None,pagina,link)
 
 # Pagina que indica si se ha indroducido un correo, un nombre, o una tarjeta valida
 @app.route('/validacion/<cadena>')
 def valido(cadena):
     nombre = request.cookies.get('nombre')
-    #visitado = ultimavisita('validacion','valido',cadena)
+    pagina = 'Validacion de : ' + str(cadena)
+    link = 'valido'
+    visitado = leerhistorial()
     if re.search('.+@\w+\.\w+', cadena):
         solucion = 'La cadena introducida es un correo'
     elif re.search('\d{4}-|\s\d{4}-|\s\d{4}-|\s\d{4}', cadena):
         solucion = 'La cadena introducida es una tarjeta valida'
     elif re.search('\w+\s\w', cadena):
-        solucion = f'La cadena introducida es un nombre'
+        solucion = 'La cadena introducida es un nombre'
     else :
-        solucion = f'la cadena introducida no corresponde a un correo,tarjeta valida o nombre'
+        solucion = 'la cadena introducida no corresponde a un correo,tarjeta valida o nombre'
 
-    return render_template('index.html',solucion=solucion,nombre=nombre)
+    return enviarcookie(render_template('index.html',solucion=solucion,nombre=nombre,visitado=visitado),None,pagina,link,cadena)
 
 @app.route('/salir')
 def salir():
-    resp = make_response(render_template('index.html'))
-    resp.set_cookie('nombre', expires=0)
-    return resp
+    visitado = leerhistorial()
+    solucion = 'Se ha salido de la sesion'
+    
+    return enviarcookie(render_template('index.html',solucion=solucion,visitado=visitado),0)
 
 def enviarcookie(respuesta,user=None,nombrepag=None,link=None,datos=None):
     resp = make_response(respuesta)
     if user != None:
-        resp.set_cookie('nombre',user)
+        if user == 0:
+            resp.set_cookie('nombre',expires=0)
+        else:
+            resp.set_cookie('nombre',user)
     if nombrepag != None and link != None:
-        clave = str(nombrepag) + ',' + str(link) + ',' + str(datos)
+        if(datos != None):
+            clave = str(nombrepag) + '/' + str(link) + '/' + str(datos)
+        else:
+            clave = str(nombrepag) + '/' + str(link)
         cookie0 = request.cookies.get('visitado0')
         cookie1 = request.cookies.get('visitado1')
         cookie2 = request.cookies.get('visitado2')
@@ -272,16 +292,25 @@ def leerhistorial():
     cookie1 = request.cookies.get('visitado1')
     cookie2 = request.cookies.get('visitado2')
     if cookie0 != None:
-        cadena = cookie0.split(',')
-        cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+        cadena = cookie0.split('/')
+        if len(cadena) == 3:
+            cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+        else:
+            cookie = {'nombre':cadena[0],'link':cadena[1]}
         resp.append(cookie)
         if cookie1 != None:
-            cadena = cookie1.split(',')
-            cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+            cadena = cookie1.split('/')
+            if len(cadena) == 3:
+                cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+            else:
+                cookie = {'nombre':cadena[0],'link':cadena[1]}
             resp.append(cookie)
             if cookie2 != None:
-                cadena = cookie2.split(',')
-                cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+                cadena = cookie2.split('/')
+                if len(cadena) == 3:
+                    cookie = {'nombre':cadena[0],'link':cadena[1],'dato':cadena[2]}
+                else:
+                    cookie = {'nombre':cadena[0],'link':cadena[1]}
                 resp.append(cookie)
     return resp
 
