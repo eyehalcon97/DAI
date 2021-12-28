8f4f1b671a4ff3847f577f736292928eacaee9b7
Manuel Ortiz <gaditanodel97@gmail.com>
2021-12-28 20:07:22 +0100

    actualizacion

--- practica5/app/galerias/urls.py
+++ practica5/app/galerias/urls.py
@@ -3,19 +3,22 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('Ver', views.Ver, name='Ver'),
+    path('ejemplo', views.ejemplo, name='ejemplo'),
+    path('VerGalerias', views.VerGalerias, name='VerCuadros'),
+    path('VerCuadros', views.VerCuadros, name='VerGalerias'),
     path('CrearCuadro', views.CrearCuadro, name='CrearCuadro'),
     path('CrearGaleria', views.CrearGaleria, name='CrearGaleria'),
-    path('EliminarGaleria',views.EliminarGaleria, name='EliminarGaleria'),
+    path('EliminarGaleria/<int:numero>',views.EliminarGaleria, name='EliminarGaleria'),
     path('EliminarCuadro',views.EliminarCuadro, name='EliminarCuadro'),
-    path('ElegirModificarGaleria',views.ElegirModificarGaleria, name='ElegirModificarGaleria'),
-    path('ModificarGaleria/<int:numero>',views.ModificarGaleria, name='ModificarGaleria'),
-    path('ElegirModificarCuadro',views.ElegirModificarCuadro, name='ElegirModificarCuadro'),
-    path('ModificarCuadro/<int:numero>',views.ModificarCuadro, name='ModificarCuadro'),
+    path('EditarGaleria/<int:numero>',views.EditarGaleria, name='EditarGaleria'),
+    path('ModificarFormularioGaleria/<int:numero>',views.ModificarFormularioGaleria, name='ModificarFormularioGaleria'),
+
+
     
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- practica5/app/galerias/views.py
+++ practica5/app/galerias/views.py
@@ -1,13 +1,20 @@
 # mi_aplicacion/views.py
 
+import logging
+
 from django.db.models.deletion import RestrictedError
+from django.http.request import HttpRequest
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render, HttpResponse
 from django.template import Template, Context, loader
 from django.middleware import csrf
+from django.http import JsonResponse
 from os import remove
+
+
 from . import forms
 from . import models
+
 
 
 def index(request):
@@ -15,14 +22,28 @@ def index(request):
         return HttpResponse( render( request, 'base.html', {'username':request.user } ) )
     return render(request, 'base.html')
 
+def ejemplo(request):
+    if request.method == 'POST':
+        return render(request,'ejemplo.html')
 
-
-def Ver(request):
+def VerGalerias(request):
     lista_galerias = models.Galeria.objects.all()
-    lista_cuadros = models.Cuadro.objects.all()
+    formcreargaleria = forms.GaleriaForm
+
+
     if request.user.is_authenticated:
-        return HttpResponse( render( request, 'ver.html', { 'galerias': lista_galerias , 'cuadros' : lista_cuadros , 'username':request.user  } ) )
-    return HttpResponse( render( request, 'ver.html', { 'galerias': lista_galerias , 'cuadros' : lista_cuadros } ))
+        return HttpResponse( render( request, 'verGalerias.html', { 'galerias': lista_galerias , 'formcreargaleria': formcreargaleria , 'username':request.user  } ) )
+    return HttpResponse( render( request, 'verGalerias.html', { 'galerias': lista_galerias ,'formcreargaleria': formcreargaleria  } ))
+
+
+
+def VerCuadros(request):
+    lista_cuadros = models.Cuadro.objects.all()
+
+    if request.user.is_authenticated:
+        return HttpResponse( render( request, 'verCuadros.html', {  'cuadros' : lista_cuadros , 'username':request.user  } ) )
+    return HttpResponse( render( request, 'verCuadros.html', {  'cuadros' : lista_cuadros } ))
+
 
 
 def CrearCuadro(request):
@@ -47,52 +68,49 @@ def CrearCuadro(request):
     
 
 def CrearGaleria(request):
-    if request.user.is_superuser:
-        if request.method == 'POST':
-            formulario = forms.GaleriaForm(request.POST)
-            if formulario.is_valid():
-                formulario.save()
-                msg = "Se ha creado la galeria"
-            else:
-                msg = "Error al crer la galeria"
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:   
-            form = forms.GaleriaForm
-            return HttpResponse(render(request, 'formularioCrear.html', {'form': form , 'username':request.user  }))
-    else:
-        msg = "No tienes permisos para realizar esta accion"
-        if request.user.is_authenticated:
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:
-            return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
+    if request.method == 'POST':
+        formulario = forms.GaleriaForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+    return HttpResponseRedirect('/VerGalerias')
 
 
-def EliminarGaleria(request):
-    if request.user.is_superuser:    
-        if request.method == 'POST':
-            formulario = forms.ElegirGaleriaForm(request.POST)
-            if formulario.is_valid():
-                numero = request.POST.get('galeria')
-                galeria = models.Galeria.objects.get(id = numero)
-                galeria.delete()
-                msg = "La galeria ha sido borrada"
-            else:
-                msg = "Error al borrar la galeria"
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
+def EliminarGaleria(request, numero):
+    if request.method == 'DELETE':
+        galeria = models.Galeria.objects.get(id=numero)
+        galeria.delete()
+    return HttpResponseRedirect('/VerGalerias')
+
+
+
+def ModificarFormularioGaleria(request, numero):
+    if request.method == 'GET':
+        galeria = models.Galeria.objects.get(id=numero)
+        formulario = forms.GaleriaForm(instance=galeria)
+        
+        return HttpResponse(formulario)
+
+
+def EditarGaleria(request,numero):
+    if request.method == 'POST':
+        galeria = models.Galeria.objects.get(id = numero)
+        formulario = forms.GaleriaForm(request.POST,instance=galeria)
+        if formulario.is_valid():
+            print("entro")
+            formulario.save()
         else:
-            if len(models.Galeria.objects.all()) == 0:
-                msg = "No hay Galerias creadas"
-                form = ""
-            else:
-                msg = "Cual de las siguientes galerias desea modificar"   
-                form = forms.ElegirGaleriaForm()
-            return HttpResponse(render(request, 'formularioEliminar.html', {'form': form , 'msg': msg , 'username':request.user }))
-    else:
-        msg = "No tienes permisos para realizar esta accion"
-        if request.user.is_authenticated:
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:
-            return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
+            
+            print("no valido")
+    print("fuera")
+    return HttpResponseRedirect('/VerGalerias')
+
+
+
+
+
+
+
+
 
 
 def EliminarCuadro(request):
@@ -124,60 +142,6 @@ def EliminarCuadro(request):
             return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
 
 
-def ElegirModificarGaleria(request):
-    if request.user.is_superuser:
-        if request.method == 'POST':
-            formulario = forms.ElegirGaleriaForm(request.POST)
-            if formulario.is_valid():
-                numero = request.POST.get('galeria')
-                galeria = models.Galeria.objects.get(id=numero)
-                url = '/ModificarGaleria/' + str(galeria.id)
-                return HttpResponseRedirect(url)
-            else:
-                msg = "Error al modificar la galeria"
-                return HttpResponse(render(request, 'base.html', { 'msg': msg , 'username':request.user }))
-        else:
-            if len(models.Galeria.objects.all()) == 0:
-                msg = "No hay Galerias creadas"
-                form = ""
-            else:
-                msg = "Cual de las siguientes galerias desea modificar"   
-                form = forms.ElegirGaleriaForm()
-            return HttpResponse(render(request, 'formularioModificar.html', {'form': form , 'msg': msg , 'username':request.user }))
-    else:
-        msg = "No tienes permisos para realizar esta accion"
-        if request.user.is_authenticated:
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:
-            return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
-
-
-def ElegirModificarCuadro(request):
-    if request.user.is_staff:
-        if request.method == 'POST':
-            formulario = forms.ElegirCuadroForm(request.POST)
-            if formulario.is_valid():
-                numero = request.POST.get('cuadro')
-                cuadro = models.Cuadro.objects.get(id = numero)
-                url = '/ModificarCuadro/' + str(cuadro.id)
-                return HttpResponseRedirect(url)
-            else:
-                msg = "Error al modificar el cuadro"
-                return HttpResponse(render(request, 'base.html', { 'msg': msg , 'username':request.user }))
-        else:
-            if len(models.Cuadro.objects.all()) == 0:
-                msg = "No hay Cuadros creados"
-                form = ""
-            else:
-                msg = "Cual de los siguientes cuadros desea modificar"   
-                form = forms.ElegirCuadroForm()
-            return HttpResponse(render(request, 'formularioModificar.html', {'form': form , 'msg': msg , 'username':request.user }))
-    else:
-        msg = "No tienes permisos para realizar esta accion"
-        if request.user.is_authenticated:
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:
-            return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
 
 
 def ModificarCuadro(request,numero):
@@ -202,23 +166,3 @@ def ModificarCuadro(request,numero):
             return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
 
 
-def ModificarGaleria(request, numero):
-    if request.user.is_superuser:
-        galeria = models.Galeria.objects.get(id = numero)
-        if request.method == 'POST':
-            formulario = forms.GaleriaForm(request.POST,instance=galeria)
-            if formulario.is_valid():
-                formulario.save()
-                msg = "La galeria ha sido modificada con exito"
-            else:
-                msg = "Error al modificar la galeria"
-            return HttpResponse(render(request, 'base.html', { 'msg': msg , 'username':request.user }))
-        else:   
-            form = forms.GaleriaForm(instance=galeria)
-            return HttpResponse(render(request, 'formularioModificar.html', {'form': form  , 'username':request.user }))#
-    else:
-        msg = "No tienes permisos para realizar esta accion"
-        if request.user.is_authenticated:
-            return HttpResponse( render( request, 'base.html', {'username':request.user , 'msg': msg } ) )
-        else:
-            return HttpResponse( render( request, 'base.html', {'msg': msg } ) )
